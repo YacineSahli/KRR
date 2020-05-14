5abeddfa36083e6b0a6250ef401e225a4dd6f466
Sujet17 <joachim.sneessens@student.umons.ac.be>
2020-05-14 22:14:24 +0200

    Generator q2

--- final/dbgen/dbq2.py
+++ final/dbgen/dbq2.py
@@ -3,7 +3,7 @@ import random
 import argparse
 import math
 
-"""q= ∃x, y, z, v, u, d (R 1 (x, y, z) ∧ R 3 (y, v) ∧ R 2 (v, u, d))
+"""q= ∃x, y, z, v, u, d (R 1 (x| y, z) ∧ R 3 (y| v) ∧ R 2 (v| u, d))
 """
 
 
@@ -16,27 +16,18 @@ def generateInstance(instance="yes", size=3, inconsistency=0, error=0):
 
 def generateYesInstance(size, inconsistency):
     res = ""
-    """ for i in range(size):
-        res += "r1(x"+str(i)+",y"+str(i)+",z)."
-        res += "r2(v"+str(i)+",u"+str(i)+",d"+str(i)+")."
-        res += "r3(y"+str(i)+",v"+str(i)+").\n"
-        r=random.random()
-        if r<inconsistency:
-            res += "r1(x"+str(i)+",y"+str(size+i)+",z)."
-            res += "r2(v"+str(i)+",u"+str(size+i)+",d"+str(size+i)+")."
-            res += "r3(y"+str(i)+",v"+str(size+i)+").\n"
-    return res """
-
-    i = 0
+    
     x1 = math.ceil(size*(1-inconsistency))
     x2 = size
     for i in range(x1):
-        res += "r1(x"+str(i)+",y"+str(i)+",z"+str(i)+")."
-        res += "r2(y"+str(i)+",v"+str(i)+",w"+str(i)+").\n"
+        res += "r1(x"+str(i)+",y"+str(i)+",z"+str(i)+").\n"
+        res += "r3(y"+str(i)+",v"+str(i)+").\n"
+        res += "r2(v"+str(i)+",u"+str(i)+",d"+str(i)+").\n"
     for i in range(x1,x2):
         r1 = random.randint(0, x1-1)
-        res += "r1(x"+str(i)+",y"+str(r1)+",z"+str(r1)+")."
-        res += "r2(y"+str(r1)+",v"+str(r1)+",w"+str(r1)+").\n"
+        res += "r1(x"+str(i)+",y"+str(r1)+",z"+str(r1)+").\n"
+        res += "r3(y"+str(r1)+",v"+str(r1)+").\n"
+        res += "r2(v"+str(r1)+",u"+str(r1)+",d"+str(r1)+").\n"
     return res
 
 
