6a770c0a322b10a3a557d2183810a543b7771def
Sujet17 <joachim.sneessens@student.umons.ac.be>
2020-05-21 15:12:35 +0200

    gt q2 and q4

--- queries/gtq4.py
+++ queries/gtq4.py
@@ -1,26 +1,25 @@
 import os
 import sys
 
-tmp_file = 'bosimfq158plkefvs.lp'
-output_file = 'q2gt.lp'
+tmp_file = 'boszuiydul75812baefvs.lp'
+output_file = 'q4gt.lp'
 
 script = """
-%q2(z,w) := ∃x, y, v (r1(x | y, z) ∧ r2(y | v, w))
+%q4(z, d) := ∃x, y, v, u (r1(x| y, z) and r3(y| v) and r2(v| u, d))
 
 1 {rr1(X,Y,Z) : r1(X,Y,Z)} 1 :- r1(X,_,_).
 1 {rr2(X,Y,Z) : r2(X,Y,Z)} 1 :- r2(X,_,_).
+1 {rr3(X,Y) : r3(X,Y)} 1 :- r3(X,_).
 
-rr(X,Y,Z,W,V) :- rr1(X,Y,Z), rr2(Y,V,W).
+rr(X,Y,Z,D,V,U) :- rr1(X,Y,Z), rr3(Y,V), rr2(V,U,D).
 
 """
-
-
 
 def build_rr_line(s :str):
     array = s.replace('q(','').replace(')','').strip('\n').split(' ')
     res = ':- '
     for var in array:
-        res += 'rr(_,_,' + var + ',_), '
+        res += 'rr(_,_,' + var + ',_,_), '
     return res[:-2] + '.\n'
     
 def write_lp_file(s: str):
@@ -28,11 +27,12 @@ def write_lp_file(s: str):
     f = open(output_file, "w")
     f.write(script)
     f.write(rr_line)
+    f.write("#show .\n")
     f.close()
     
 def write_tmp_file():
     f = open(tmp_file, "w")
-    f.write('q(Z,W) :- r1(_,_,Z),r2(_,_,W).\n #show q/2.')
+    f.write('q(Z,D) :- r1(_,_,Z), r2(_,_,D).\n #show q/2.')
     f.close()
 
 def rm_tmp_file():
