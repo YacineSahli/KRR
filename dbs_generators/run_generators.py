905b920a222c20c7b2d772eedf4364f49d695fd5
Sujet17 <joachim.sneessens@student.umons.ac.be>
2020-05-26 23:22:06 +0200

    Suppression gitignore

--- dbs_generators/run_generators.py
+++ dbs_generators/run_generators.py
@@ -6,14 +6,14 @@ inconsistency = 0.4
 
 for i in range(7):
     for j in range(1,3):
-        db_size = int(j * 1e6)
+        db_size = int(j * 1e3)
         nb_consist = int(db_size / arities[i] * 0.4)
         nb_inconsist = int(db_size / arities[i] * 0.2)
         params = [db_size, inconsistency, nb_consist, nb_inconsist]
         
         cmd = 'python3 db' + str(i+1) + '.py ' + ' '.join(str(p) for p in params)
         db_file = '../generated_dbs/q' + str(i+1) + '_' + str(j)\
-                  + 'M_' + '_' + str(inconsistency) + '_'\
+                  + '000_' + '_' + str(inconsistency) + '_'\
                   + "{:.2f}".format(nb_inconsist/(nb_consist+nb_inconsist)) + '.lp'
         os.system(cmd + ' > ' + db_file)
         
