1997f08714e13919f607e34e488f203da30db88f
LathanDevers <lathandevers@outlook.com>
2020-05-26 09:54:53 +0200

    merge

--- generateGraphs.R
+++ generateGraphs.R
@@ -7,7 +7,7 @@ y2<-c(24.524, 53.354, 83.556, 114.058, 114.058+(83.556-53.354))
 
 x<-1:5
 ggplot(data.frame(x,y1,y2),aes(x))+
-  ggtitle("cpu : 20% inc / yes instance / q1 (red fo, green g&t)")+
+  ggtitle("cpu : 20% inc / yes instance / q3 (red fo, green g&t)")+
   xlab("entries (Million)")+
   ylab("time (sec)")+
   geom_line(aes(y=y1), colour="green")+
@@ -18,12 +18,12 @@ ggplot(data.frame(x,y1,y2),aes(x))+
 
 #y1<-c(26.327, 55.969, 87.699,87.699+(55.969-26.327),87.699+2*(55.969-26.327))
 #y2<-c(11.680, 24.444, 37.046, 49.055, 61.611)
-y1<-c(144.905, 203.318, 203.318)
-y2<-c(41.560, 93.013, 133.081, 194.293)
+y1<-c(144.905, 203.318, 203.318+144.905, 203.318+2*144.905, 203.318+3*144.905)
+y2<-c(41.560, 93.013, 133.081, 194.293, 194.293+(133.081-93.013))
 
 x<-1:5
 ggplot(data.frame(x,y1,y2),aes(x))+
-  ggtitle("cpu : 20% inc / no instance / q1 (red fo, green g&t)")+
+  ggtitle("cpu : 20% inc / no instance / q3 (red fo, green g&t)")+
   xlab("entries (Million)")+
   ylab("time (sec)")+
   geom_line(aes(y=y1), colour="green")+
@@ -38,7 +38,7 @@ y2<-c(2600002, 5200002, 7800002, 10400002, 10400002+(7800002-5200002))
 
 x<-1:5
 ggplot(data.frame(x,y1,y2),aes(x))+
-  ggtitle("rules : 20% inc / yes instance / q1 (red fo, green g&t)")+
+  ggtitle("rules : 20% inc / yes instance / q3 (red fo, green g&t)")+
   xlab("entries (Million)")+
   ylab("rules")+
   geom_line(aes(y=y1), colour="green")+
@@ -53,10 +53,11 @@ y2<-c(4600001, 9200001, 13800001, 18400001, 18400001+(13800001-9200001))
 
 x<-1:5
 ggplot(data.frame(x,y1,y2),aes(x))+
-  ggtitle("rules : 20% inc / no instance / q1 (red fo, green g&t)")+
+  ggtitle("rules : 20% inc / no instance / q3 (red fo, green g&t)")+
   xlab("entries (Million)")+
   ylab("rules")+
   geom_line(aes(y=y1), colour="green")+
   geom_line(aes(y=y2), colour="red")+
   geom_point(aes(y=y1), colour="green")+
   geom_point(aes(y=y2), colour="red")
+
